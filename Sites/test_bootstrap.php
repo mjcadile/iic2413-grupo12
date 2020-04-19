4df581cdaf4497fe9f57013290fa1c58ef336a8d
Matias Cadile <mjcadile@uc.cl>
2020-04-19 17:33:27 -0400

    prueba bootstrap

---
+++ Sites/test_bootstrap.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="Content-Type" content="text/html">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Prueba de Bootstrap</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</head>
+<body>
+
+<div class="container">
+  <div class="panel panel-default">
+    <div class="panel-heading text-center">
+      <h2>DCConsultas</h2>
+    </div>
+    <div class="panel-body">
+      <h4>Consulta 1</h4>
+      <input type="text" class="form-control" name="Consulta 1">
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
