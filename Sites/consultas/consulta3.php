5cfcaf76a3b0b729a5e3aaa85980326c588f7795
Matias Cadile <mjcadile@uc.cl>
2020-04-20 13:35:50 -0400

    arreglo de errores

--- Sites/consultas/consulta3.php
+++ Sites/consultas/consulta3.php
@@ -7,16 +7,16 @@
 
   #Se obtiene el valor del input del usuario
   $pais = $_POST["pais"];
-  $searchVal = array("á", "é", "í", "ó", "ú", 'Á', "É", "Í", "Ó", "Ú")
-  $replaceVal = array("a", "e", "i", "o", "u", "A", "E", "I", "O", "U")
+  $searchVal = array("á", "é", "í", "ó", "ú", 'Á', "É", "Í", "Ó", "Ú");
+  $replaceVal = array("a", "e", "i", "o", "u", "A", "E", "I", "O", "U");
   $pais = str_replace($searchVal, $replaceVal, $pais);
-  $pais= strtolower($pais)
+  $pais= strtolower($pais);
 
   #Se construye la consulta como un string
   $query = "SELECT DISTINCT Museos.nombre FROM (SELECT Lugares.nombre, Lugares.cid FROM 
   Museos, Obras, Lugares WHERE Obras.periodo = 'Renacimiento' AND Obras.lid = Museos.lid AND 
   Lugares.lid = Museos.lid) as Museos,  Ciudades, Paises WHERE  Museos.cid = Ciudades.cid AND 
-  Ciudades.pid = Paises.pid AND LOWER(Paises.nombre) LIKE LOWER('%$pais%') ;"
+  Ciudades.pid = Paises.pid AND LOWER(Paises.nombre) LIKE LOWER('%$pais%');";
  	#$query = "SELECT id, nombre, altura FROM ejercicio_ayudantia where altura>=$altura order by altura desc;";
 
   #Se prepara y ejecuta la consulta. Se obtienen TODOS los resultados
