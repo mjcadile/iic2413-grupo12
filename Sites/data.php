2c27146256172c5b73fb832a9b1e034a88e082c2
Matias Cadile <mjcadile@uc.cl>
2020-04-20 00:19:19 -0400

    avance

---
+++ Sites/data.php
@@ -0,0 +1,5 @@
+<?php
+  $user = 'grupo12';
+  $password = 'grupo12';
+  $databaseName = 'grupo12e2';
+?>
---
+++ Sites/index_copia.php
@@ -0,0 +1,99 @@
+<?php include('templates/header.html');   ?>
+
+<body>
+  <h1 align="center">DCConsultas </h1>
+  <p class="lead" style="text-align:center;">En esta página podrás encontrar información sobre obras de arte
+  alrededor del mundo. Hecho por José Baboun y Matías Cadile, grupo12</p>
+
+  <br>
+
+  <h3 align="center"> Mostrar todos los nombres distintos de las obras de arte:</h3>
+
+  <form align="center" action="consultas/consulta1.php" method="post">
+    <input type="submit" class="btn btn-primary" value="Consultar">
+  </form>
+  
+  <br>
+  <br>
+  <br>
+
+  <h3 align="center"> Mostrar todos los nombres de las plazas que 
+          contengan al menos una escultura de "Gian Lorenzo Bernini":</h3>
+
+  <form align="center" action="consultas/consulta2.php" method="post">
+    <br/><br/>
+    <input type="submit" class="btn btn-primary" value="Consultar">
+  </form>
+  
+  <br>
+  <br>
+  <br>
+
+  <h3 align="center"> A continuación se muestra el nombre de todos los museos 
+          de un país que tenga obras del renacimiento.</h3>
+
+  <form align="center" action="consultas/consulta3.php" method="post">
+    Ingrese el nombre del país que quiera consultar:
+    <input type="text" name="pais">
+    <br/><br/>
+    <input type="submit" class="btn btn-primary" value="Consultar">
+  </form>
+  <br>
+  <br>
+  <br>
+
+  <h3 align="center">Para cada artista, se despliega su nombre y el número de obras en las que ha participado.</h3>
+
+  <?php
+  #Primero obtenemos todos los tipos de pokemones
+  #require("config/conexion.php");
+  #$result = $db -> prepare("SELECT DISTINCT tipo FROM ejercicio_ayudantia;");
+  #$result -> execute();
+  #$dataCollected = $result -> fetchAll();
+  ?>
+
+  <form align="center" action="consultas/consulta4.php" method="post">
+    <br><br>
+    <input type="submit" class="btn btn-primary" value="Consultar">
+  </form>
+
+  <br>
+  <br>
+  <br>
+  <br>
+
+  <h3 align="center"> A continuación se muestran los nombres de las iglesias 
+          ubicadas en cierta ciudad, abiertas entre ciertas horas (inclusive) 
+          junto a todos los nombres de los frescos que se encuentran en cada una de ellas.</h3>
+
+  <form align="center" action="consultas/consulta5.php" method="post">
+    Ingrese una hora de apertura (formato hh:mm:ss):
+    <input type="text" name="h_apertura">
+    <br/><br/>
+    Ingrese una hora de cierre (formato hh:mm:ss):
+    <input type="text" name="h_cierre">
+    <br/><br/>
+    Ingrese una ciudad:
+    <input type="text" name="ciudad">
+    <br/><br/>
+    <input type="submit" class="btn btn-primary" value="Consultar">
+  </form>
+  
+  <br>
+  <br>
+  <br>
+
+  <h3 align="center"> Se despliega el nombre de cada museo, plaza o iglesia 
+          que tenga obras de todos los periodos del arte que existan en nuestro registro</h3>
+
+  <form align="center" action="consultas/consulta6.php" method="post">
+    <br/><br/>
+    <input type="submit" class="btn btn-primary" value="Consultar">
+  </form>
+  
+  <br>
+  <br>
+  <br>
+  
+</body>
+</html>
