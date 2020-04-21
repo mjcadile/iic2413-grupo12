46b65fd47144816d4a478fe0ece610af02d5efb4
josebaboun <42177793+josebaboun@users.noreply.github.com>
2020-04-21 09:33:20 -0400

    background

--- Sites/index.php
+++ Sites/index.php
@@ -2,15 +2,15 @@
 # http://codd.ing.puc.cl/~grupo12/index_copia.php? ?>
 
 <body>
-<img src="https://i.pinimg.com/564x/97/b4/36/97b43628eff3af035ea54ea218bd936a.jpg" id="bg" alt="">
+<img src="http://www.pxleyes.com/images/tutorials/submissionsteps/fullsize/1658_step3_4b35a144b19b5.jpg" id="bg" alt="">
 
   <h1 align="center" style="font-size:50px;color:white;">DCConsultas </h1>
   <p class="lead" style="text-align:center; color:white;">En esta página podrás encontrar información sobre obras de arte
   alrededor del mundo. <br>Hecho por José Baboun y Matías Cadile. <br/>Grupo12</p>
 
   <br>
-
-  <h4 align="center"> Mostrar todos los nombres distintos de las obras de arte:</h4>
+  <hr>
+  <h4 align="center" style="color:white;"> Mostrar todos los nombres distintos de las obras de arte:</h4>
 
   <form align="center" action="consultas/consulta1.php" method="post">
     <input type="submit" class="btn btn-primary" value="Consultar">
@@ -20,7 +20,7 @@
   <br>
   <br>
 
-  <h4 align="center"> Mostrar todos los nombres de las plazas que 
+  <h4 align="center" style="color:white;"> Mostrar todos los nombres de las plazas que 
           contengan al menos una escultura de "Gian Lorenzo Bernini":</h4>
 
   <form align="center" action="consultas/consulta2.php" method="post">
@@ -32,7 +32,7 @@
   <br>
   <br>
 
-  <h4 align="center"> A continuación se muestra el nombre de todos los museos 
+  <h4 align="center" style="color:white;"> A continuación se muestra el nombre de todos los museos 
           de un país que tenga obras del renacimiento.</h4>
 
   <form align="center" action="consultas/consulta3.php" method="post">
@@ -45,7 +45,7 @@
   <br>
   <br>
 
-  <h4 align="center">Para cada artista, se despliega su nombre y el número de obras en las que ha participado.</h4>
+  <h4 align="center" style="color:white;">Para cada artista, se despliega su nombre y el número de obras en las que ha participado.</h4>
 
   <?php
   #Primero obtenemos todos los tipos de pokemones
@@ -65,7 +65,7 @@
   <br>
   <br>
 
-  <h4 align="center"> A continuación se muestran los nombres de las iglesias 
+  <h4 align="center" style="color:white;"> A continuación se muestran los nombres de las iglesias 
           ubicadas en cierta ciudad, abiertas entre ciertas horas (inclusive) 
           junto a todos los nombres de los frescos que se encuentran en cada una de ellas.</h4>
 
@@ -86,7 +86,7 @@
   <br>
   <br>
 
-  <h4 align="center"> Se despliega el nombre de cada museo, plaza o iglesia 
+  <h4 align="center" style="color:white;"> Se despliega el nombre de cada museo, plaza o iglesia 
           que tenga obras de todos los periodos del arte que existan en nuestro registro</h4>
 
   <form align="center" action="consultas/consulta6.php" method="post">
