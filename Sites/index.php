a47b377db3d86cfc427ec3abcf0e57f3590aadd0
MATIAS CADILE <mjcadile@uc.cl>
2020-04-21 18:50:45 -0400

    ultimos cambios

--- Sites/index.php
+++ Sites/index.php
@@ -7,7 +7,7 @@ hr.new1 {
 </style>
 <body>
 
-    <div class="card">
+    <div class="card" style="max-height: 18rem;">
     <img class="card-img-top" src="https://upload.wikimedia.org/wikipedia/commons/5/5b/Michelangelo_-_Creation_of_Adam_%28cropped%29.jpg" alt="Card image cap">
     <div class="card-body">
         <h2 class="card-title">DCConsultas</h2>
@@ -17,7 +17,7 @@ hr.new1 {
 
 
 <!--Consulta1!-->
-    <div class="card border-info mb-3" style="width: 40rem;">
+    <div class="card border-info mb-3">
         <div class="card-header">
             Consulta 1
         </div>
@@ -34,7 +34,7 @@ hr.new1 {
         </div>
     </div>
 <!--Consulta2!-->
-    <div class="card text-center border-info mb-3" style="width: 40rem;">
+    <div class="card text-center border-info mb-3">
         <div class="card-header">
             Consulta 2
         </div>
@@ -51,7 +51,7 @@ hr.new1 {
         </div>
     </div>
 <!--Consulta3!-->
-    <div class="card text-right border-info mb-3" style="width: 40rem;">
+    <div class="card text-right border-info mb-3">
         <div class="card-header">
             Consulta 3
         </div>
@@ -71,7 +71,7 @@ hr.new1 {
         </div>
     </div>
 <!--Consulta4!-->
-    <div class="card text-center border-info mb-3" style="width: 40rem;">
+    <div class="card text-center border-info mb-3">
             <div class="card-header">
                 Consulta 4
             </div>
@@ -88,7 +88,7 @@ hr.new1 {
             </div>
         </div>
 <!--Consulta5!-->
-    <div class="card border-info mb-3" style="width: 40rem;">
+    <div class="card border-info mb-3">
             <div class="card-header">
                 Consulta 5
             </div>
@@ -119,7 +119,7 @@ hr.new1 {
             </div>
     </div>
 <!--Consulta6!-->
-    <div class="card text-center border-info mb-3" style="width: 40rem;">
+    <div class="card text-center border-info mb-3">
             <div class="card-header">
                 Consulta 6
             </div>
