d0a4451f25562150bb624ab37dd7bea9584338ca
josebaboun <42177793+josebaboun@users.noreply.github.com>
2020-05-10 17:40:54 -0400

    a

--- Sites/consultas_e3/consulta_artistas.php
+++ Sites/consultas_e3/consulta_artistas.php
@@ -10,12 +10,12 @@
       $seleccionado = $_POST["aid"];
       $seleccionado = number_format($seleccionado);
       #Se construye la consulta como un string
-      $query = 'SELECT * FROM Artistas WHERE Artistas.aid = 2';
+      $query = 'SELECT * FROM Artistas WHERE Artistas.aid = '%$seleccionado%'';
      
    
       $query_obras = 'SELECT Obras.oid, Obras.nombre, Obras.ano_inicio, Obras.ano_termino, Obras.periodo
       FROM Artistas, Hecha_por, Obras WHERE Artistas.aid = Hecha_por.aid 
-      AND Hecha_por.oid = Obras.oid AND Artistas.aid = 2 ';
+      AND Hecha_por.oid = Obras.oid AND Artistas.aid = '%$seleccionado%' ';
 
       #Se prepara y ejecuta la consulta. Se obtienen TODOS los resultados
 	    $result = $db -> prepare($query);
