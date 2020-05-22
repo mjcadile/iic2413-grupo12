4f744130a45d6bb9702ab851b76717f055d308f9
Matias Cadile <mjcadile@uc.cl>
2020-05-22 19:33:43 -0400

    navBar

--- Sites/index.php
+++ Sites/index.php
@@ -14,7 +14,32 @@ hr.new1 {
             <p class="card-text">Proyecto realizado por José Baboun y Matías Cadile, IIC2413.</p>
         </div>
     </div>
-
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Consultas
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="#">Consulta 1</a>
+          <a class="dropdown-item" href="#">Consulta 2</a>
+          <a class="dropdown-item" href="#">Consulta 3</a>
+          <a class="dropdown-item" href="#">Consulta 4</a>
+          <a class="dropdown-item" href="#">Consulta 5</a>
+          <a class="dropdown-item" href="#">Consulta 6</a>
+        </div>
+      </li>
+    </ul>
+  </div>
+</nav>
 
 <!--Consulta1!-->
     <div class="card text-center border-info mb-3">
