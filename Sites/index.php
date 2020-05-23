41b2f9a100c6dbbaada4710f9f93d3a042f701d6
Matias Cadile <mjcadile@uc.cl>
2020-05-23 03:35:50 -0400

    botones login y register

--- Sites/index.php
+++ Sites/index.php
@@ -15,12 +15,11 @@ hr.new1 {
             <p class="card-text">Proyecto realizado por José Baboun y Matías Cadile, IIC2413.</p>
         </div>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="#">Bienvenidos</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul class="navbar-nav">
+                <ul class="navbar-nav mr-auto">
                     <li class="nav-item active">
                         <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
                     </li>
@@ -31,19 +30,20 @@ hr.new1 {
                         <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
                         <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
                     </form>
-                <!--li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Consultas
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                    <a class="dropdown-item" href="consultas_e3/lista_artistas.php">Consulta 1</a>
-                    <a class="dropdown-item" href="#">Consulta 2</a>
-                    <a class="dropdown-item" href="#">Consulta 3</a>
-                    <a class="dropdown-item" href="#">Consulta 4</a>
-                    <a class="dropdown-item" href="#">Consulta 5</a>
-                    <a class="dropdown-item" href="#">Consulta 6</a>
-                    </div>
-                </li-->
+                </ul>
+                <ul class="navbar-nav">
+                    <form class="form-inline my-2 my-lg-0">
+                        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                    </form>
+                </ul>
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="consultas_e2.php">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="consultas_e2.php">Register</a>
+                    </li>
                 </ul>
             </div>
         </nav>
