cb7e1fa6f7ddf3335a8f13fb9c0211e2e8c515a1
Richard Morales <rgmorales1@uc.cl>
2020-05-24 22:29:53 -0400

    Config con las 2 base de datos

--- Sites/config/conexion.php
+++ Sites/config/conexion.php
@@ -3,7 +3,15 @@
     #Pide las variables para conectarse a la base de datos.
     require('data.php'); 
     # Se crea la instancia de PDO
-    $db = new PDO("pgsql:dbname=$databaseName;host=localhost;port=5432;user=$user;password=$password");
+    $db_12 = new PDO("pgsql:dbname=$databaseName12;host=localhost;port=5432;user=$user12;password=$password12");
+  } catch (Exception $e) {
+    echo "No se pudo conectar a la base de datos: $e";
+  }
+  try {
+    #Pide las variables para conectarse a la base de datos.
+    require('data.php'); 
+    # Se crea la instancia de PDO
+    $db_19 = new PDO("pgsql:dbname=$databaseName19;host=localhost;port=5432;user=$user19;password=$password19");
   } catch (Exception $e) {
     echo "No se pudo conectar a la base de datos: $e";
   }
--- Sites/config/data.php
+++ Sites/config/data.php
@@ -1,5 +1,8 @@
 <?php
-  $user = 'grupo12';
-  $password = 'grupo12';
-  $databaseName = 'grupo12e2';
+  $user19 = 'grupo19';
+  $password19 = 'grupo19321';
+  $databaseName19 = 'grupo19e3';
+  $user12 = 'grupo12';
+  $password12 = 'grupo12';
+  $databaseName12 = 'grupo12e3';
 ?>
