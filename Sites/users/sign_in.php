f571fcb870568665258f76b69232fb0d24985027
Richard Morales <rgmorales1@uc.cl>
2020-05-24 21:30:12 -0400

    Usuarios subidos

--- Sites/users/sign_in.php
+++ Sites/users/sign_in.php
@@ -5,34 +5,45 @@ session_start();
 
 <body>
 
-<h1 align="center"> Iniciar Sesion </h1>
+<img src="https://cdn.pixabay.com/photo/2014/11/02/10/41/aircraft-513641_1280.jpg" id="bg" alt="">
+    <div class="card border-info mb-4">
+        <img class="card-img-top" src="https://cdn.pixabay.com/photo/2014/11/02/10/41/aircraft-513641_1280.jpg" alt="Card image cap">
+        <div class="card-body">
+            <h2 class="card-title">Iniciar sesion</h2>
+        </div>
+    </div>
 
-  <br>
-
-  <form align="center" action="iniciar_sesion.php" method="post">
-    Username:
-    
-    <input type="text" name="username">
-    <br/><br/>
-
-    Contraseña:
-    <input type="password" name="contrasena">
-    <br/><br/>
-    <?php if (! empty($_SESSION['user'])){
-        if ($_SESSION['user'] == 'Contraseña erronea'){
-            echo "Contraseña erronea";
-        }elseif ($_SESSION['user'] == 'Usuario no encontrado'){
-            echo "Usuario no registrado";
-        }
-    }?>
-
-  <input type="submit" value="Iniciar sesion">
-  </form>
- 
-  <br>
-  <br>
-  <br>
+    <div class="card text-center border-info mb-3">
+        <div class='card-body'>
+        <form align='center' action='iniciar_sesion.php' method='post'>
+                    <p class="card-text"> Username </p>
+                    <input type="text" name="username">
+                    <p class="card-text"> Contraseña </p>
+                    <input type="password" name="contrasena">
+                    <p class="card-text">  </p>
+                    <p class='card-text'> <?php if (! empty($_SESSION['user'])){
+                          if ($_SESSION['user'] == 'Contraseña erronea'){
+                              echo  "Contraseña erronea ";
+                          }elseif ($_SESSION['user'] == 'Usuario no encontrado'){
+                              echo "Usuario no registrado";
+                          }
+                      }?> </p>
+                    <input type='submit' class='btn btn-primary mt-2 mb-2' value='Iniciar Sesion' />
+                    
+        </form>
+        </div>
+    </div>
+</div>
+<form action="../index.php" method="get">
+    <input type="submit" class="btn btn-primary mt-8 mb-5" value="Menú Principal">
+</form>
+</div>
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<script src="../js/actions.js"></script>
+<script src="js/actions.js"></script>
 
 </body>
+
 </html>
-<?php include('../templates/footer.html'); ?>
--- Sites/users/sign_up.php
+++ Sites/users/sign_up.php
@@ -5,49 +5,56 @@ session_start();
 
 <body>
 
+<img src="https://cdn.pixabay.com/photo/2014/11/02/10/41/aircraft-513641_1280.jpg" id="bg" alt="">
+    <div class="card border-info mb-4">
+        <img class="card-img-top" src="https://cdn.pixabay.com/photo/2014/11/02/10/41/aircraft-513641_1280.jpg" alt="Card image cap">
+        <div class="card-body">
+            <h2 class="card-title">Registrarse</h2>
+        </div>
+    </div>
 
-<h1 align="center"> Registrate </h1>
-
-    <br>
-
-    <form align="center" action="registrarse.php" method="post">
-      Nombre:
-      <input type="text" name="nombre">
-      <?php 
-      if (!empty($_SESSION['user'])){
-          if ($_SESSION['user'] == "error username"){
-            echo "Username se encuentra ocupado, intente con otro.";
-          }
-      }?>
-    <br/><br/>
-    Username:
-    <input type="text" name="username">
-    <br/><br/>
-    Correo:
-    <input type="text" name="correo">
-    <br/><br/>
-    Dirección:
-    <input type="text" name="direccion">
-    <br/><br/>
-    Contraseña:
-    <input type="password" name="contrasena">
-    <br/><br/>
-    Repita contraseña:
-    <input type="password" name="contrasena_confirm">
-    <br/><br/>
-    <?php if (! empty($_SESSION['user'])){
-      if ($_SESSION['user'] == "error contraseña"){
-        echo "Las contraseñas no coinciden, intente denuevo.";
-      }
-    }?>
-    <input type="submit" value="Registrarse">
-  </form>
-
-  
-    <form action="../index.php" method="get">
-      <input type="submit" class="btn btn-primary mt-8 mb-5" value="Menú principal">
-    </form>
+    <div class="card text-center border-info mb-3">
+        <div class='card-body'>
+        <form align='center' action='registrarse.php' method='post'>
+                    <p class="card-text"> Nombre </p>
+                    <input type="text" name="nombre">
+                    <p class="card-text"> Username </p>
+                    <input type="text" name="username">
+                    <p class='card-text'>  <?php if (!empty($_SESSION['user'])){
+                        if ($_SESSION['user'] == "error username"){
+                            echo "Username se encuentra ocupado, intente con otro.";
+                        }
+                    }?> </p>
+                    <p class="card-text"> Correo </p>
+                    <input type="text" name="correo">
+                    <p class="card-text"> Dirección </p>
+                    <input type="text" name="direccion">
+                    <p class="card-text"> Contraseña </p>
+                    <input type="password" name="contrasena">
+                    <p class="card-text"> Repita la contraseña </p>
+                    <input type="password" name="contrasena_confirm">
+                    <p class="card-text">  </p>
+                    <p class='card-text'>  <?php if (! empty($_SESSION['user'])){
+                        if ($_SESSION['user'] == "error contraseña"){
+                            echo "Las contraseñas no coinciden, intente denuevo.";
+                        }
+                    }?> </p>
+                    <input type='submit' class='btn btn-primary mt-2 mb-2' value='Registrarse' />
+                    
+        </form>
+        </div>
+    </div>
+</div>
+<form action="../index.php" method="get">
+    <input type="submit" class="btn btn-primary mt-8 mb-5" value="Menú Principal">
+</form>
+</div>
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<script src="../js/actions.js"></script>
+<script src="js/actions.js"></script>
 
 </body>
+
 </html>
-<?php include('../templates/footer.html'); ?>
