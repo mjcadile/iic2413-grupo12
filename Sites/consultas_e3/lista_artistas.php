e0ceb4b3bf51824ae752b0b8e14a11b454faf1a3
josebaboun <42177793+josebaboun@users.noreply.github.com>
2020-05-26 15:50:04 -0400

    names

--- Sites/consultas_e3/lista_artistas.php
+++ Sites/consultas_e3/lista_artistas.php
@@ -35,8 +35,8 @@
                           <td>$n[1]</td>
                           <td>
                           <form action='consulta_artistas.php' method='post' >
-                              <input type = 'hidden' name = 'nombre' id = 'nombre' value = $n[1] >
-                              <input type = 'hidden' name = 'aid' id = aid value = str_replace(' ', '%', $n[0]) >
+                              <input type = 'hidden' name = 'nombre' id = 'nombre' value = str_replace(' ', '%', $n[1]) >
+                              <input type = 'hidden' name = 'aid' id = aid value = $n[0] >
                               <input class='btn btn-primary' type='submit' value='CONSULTAR'>
                           </form>
                           </td>
