4d3547736eeae82ca41e804066251f127b9b103f
Richard Morales <rgmorales1@uc.cl>
2020-05-27 01:30:56 -0400

    CE

---
+++ Sites/consultas_e3/consulta_intinerario.php
@@ -0,0 +1,67 @@
+<?php include('../templates/header_sin_searchbox.html');   ?>
+
+<body>
+  <img src="https://d2jv9003bew7ag.cloudfront.net/uploads/MoCP-Chicago.jpg" id="bg" alt="">
+    <?php
+      require("../config/conexion.php");
+
+      $fecha_actual = date("Y-m-d", time());
+      #Se construye la consulta como un string
+ 	    $query = "SELECT * FROM Artistas;";
+   
+      #Se prepara y ejecuta la consulta. Se obtienen TODOS los resultados
+	    $result = $db_12 -> prepare($query);
+	    $result -> execute();
+	    $nombres = $result -> fetchAll();
+    ?>
+
+    <div class="container-fluid mt-10">
+      <h2 class="text-center rounded-bottom bg-info text-white mb-8">Todos los artistas</h2>
+      <div class="scrollable">
+      <?php echo "
+        <p class='card-text'> Ciudad </p>
+            <input type='text' name='ciudad'>
+        <p class='card-text'> Fecha </p>
+            <input type='date' id='fecha' name='fecha'
+                  value='$fecha_actual'
+                  min='$fecha_actual' max='2025-12-31'>"?>
+        <div class="table-responsive">
+          <table class="table table-bordered table-hover table-striped text-center table-dark">
+            <thead>
+              <tr>
+                <th class="text-white bg-danger" scope="col">Nombre Artista</th>
+                <th color = 'red' class="text-white bg-warning" scope="col">Consultar</th>
+
+              </tr>
+            </thead>
+            <tbody>
+              
+              <?php
+                foreach ($nombres as $n) {
+                  $nombre = str_replace(' ', '+', $n[1]);
+                  echo "<tr class='bg-dark'>
+                          <td>$n[1]</td>˛
+                          <td>
+                          <form action='consulta_artistas.php' method='post' >
+                              <input type = 'hidden' name = 'nombre' id = 'nombre' value = $nombre >
+                              <input type = 'hidden' name = 'aid' id = aid value = $n[0] >
+                              <input type='checkbox'>
+                          </form>
+                          </td>
+                        </tr>";
+                }
+  
+              ?>
+            </tbody>
+          </table>
+          <input type='submit' class='btn btn-primary mt-2 mb-2' value='Consultar' />
+        </div>
+      </div>
+    </div>
+    <form action="../index.php" method="get">
+      <input type="submit" class="btn btn-primary mt-8 mb-5" value="Menú Principal">
+    </form>
+  </div>
+   <?php #include("templates/footer.html");?>
+
+  
