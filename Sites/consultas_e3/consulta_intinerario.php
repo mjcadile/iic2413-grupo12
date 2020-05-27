fe95c3eb2b36733ae1a7a803edb578e71b4d529e
Richard Morales <rgmorales1@uc.cl>
2020-05-27 16:21:34 -0400

    CE

--- Sites/consultas_e3/consulta_intinerario.php
+++ Sites/consultas_e3/consulta_intinerario.php
@@ -62,7 +62,9 @@
         </div>
       </div>
     </div>
-        <input type='submit' class='btn btn-primary mt-8 mb-5' value='Consultar' />
+    <div class="card text-center border-info mb-3">
+        <input type='submit' class='btn btn-primary mt-2 mb-2' value='Consultar' />
+    </div>
     </form>
     <form action="../index.php" method="get">
       <input type="submit" class="btn btn-primary mt-8 mb-5" value="Menú Principal">
