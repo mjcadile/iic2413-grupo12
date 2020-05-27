9d4739a1d03f2d27dedb887b8fc495730fe236ac
Richard Morales <rgmorales1@uc.cl>
2020-05-27 17:03:38 -0400

    CE

--- Sites/consultas_e3/consulta_intinerario.php
+++ Sites/consultas_e3/consulta_intinerario.php
@@ -33,10 +33,11 @@ if (isset($_SESSION['user']) && $_SESSION['user'] != "Contraseña erronea" &&
           <form align="center" action="hacer_intinerario.php" method="post">
               <h5 class="text-center rounded-bottom bg-info text-white mb-8">Escoge la ciudad y la fecha de la consulta.</h5>
               <p class='card-text'> Ciudad </p>
-                  <select name='selecciona la ciudad'>
-                  <?php foreach ($ciudades as $c){
-                      echo "<option value='$c[0]'>$c[0]</option>";             
-                  }?>
+                  <select name='Ciudad'>
+                      <option value='0'>Seleciona la ciudad de origen</option>
+                      <?php foreach ($ciudades as $c){
+                          echo "<option value='$c[0]'>$c[0]</option>";             
+                      }?>
                   </select>
               <?php echo "
                 <p class='card-text'> Fecha </p>
