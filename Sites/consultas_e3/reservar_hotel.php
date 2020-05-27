b340c842400c69920130eeb3e26fcc17b4cc5ae6
Richard Morales <rgmorales1@uc.cl>
2020-05-26 23:01:20 -0400

    CE

--- Sites/consultas_e3/reservar_hotel.php
+++ Sites/consultas_e3/reservar_hotel.php
@@ -10,30 +10,31 @@
     $hid = number_format($hid);
     $fecha_entrada = $_POST["start"];
     $fecha_salida = $_POST["finish"];
+    if (strtotime($fecha_entrada) <= strtotime($fecha_entrada)){
+        $username = $_SESSION['user'];
+        $query_uid = "SELECT uid FROM Usuarios WHERE Usuarios.username = '$username';";
+        $result = $db_19 -> prepare($query_uid);
+        $result -> execute();
+        $uid_usuario = $result -> fetchAll();
+        foreach ($uid_usuario as $u){
+            $uid = number_format(intval($u[0]));
+        }
 
-    $username = $_SESSION['user'];
-    $query_uid = "SELECT uid FROM Usuarios WHERE Usuarios.username = '$username';";
-    $result = $db_19 -> prepare($query_uid);
-    $result -> execute();
-    $uid_usuario = $result -> fetchAll();
-    foreach ($uid_usuario as $u){
-        $uid = number_format(intval($u[0]));
+        $query_max = "SELECT MAX(rid) FROM Reservas;";
+        $resultado = $db_19 -> prepare($query_max);
+        $resultado -> execute();
+        $rid_max = $resultado -> fetchAll();
+        foreach ($rid_max as $r){
+            $rid = number_format(intval($r[0]) + 1);
+        }
+
+        $query = "INSERT INTO Reservas VALUES('$rid', '$hid', '$uid', '$fecha_entrada', '$fecha_salida');";
+        $agregar = $db_19 -> prepare($query);
+        $agregar -> execute();
+        $resultado = $agregar -> fetchAll();
+        header('Status: 301 Moved Permanently', false, 301);
+        header('Location: ../index.php');
+        header("Connection: close");
+        exit();
     }
-
-    $query_max = "SELECT MAX(rid) FROM Reservas;";
-    $resultado = $db_19 -> prepare($query_max);
-    $resultado -> execute();
-    $rid_max = $resultado -> fetchAll();
-    foreach ($rid_max as $r){
-        $rid = number_format(intval($r[0]) + 1);
-    }
-
-    $query = "INSERT INTO Reservas VALUES('$rid', '$hid', '$uid', '$fecha_entrada', '$fecha_salida');";
-    $agregar = $db_19 -> prepare($query);
-    $agregar -> execute();
-    $resultado = $agregar -> fetchAll();
-    header('Status: 301 Moved Permanently', false, 301);
-    header('Location: ../index.php');
-    header("Connection: close");
-    exit();
 ?>
