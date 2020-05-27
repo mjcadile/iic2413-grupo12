cbef184bb60ce71437eb3549fca44ba0ce5e27da
Richard Morales <rgmorales1@uc.cl>
2020-05-27 16:30:01 -0400

    CE

--- Sites/consultas_e3/hacer_intinerario.php
+++ Sites/consultas_e3/hacer_intinerario.php
@@ -1,4 +1,12 @@
-<?php include('../templates/header_sin_searchbox.html');   ?>
+<?php 
+session_start();
+if (isset($_SESSION['user']) && $_SESSION['user'] != "Contraseña erronea" && 
+        $_SESSION['user'] != "Usuario no encontrado" && $_SESSION['user'] != "error username" && 
+        $_SESSION['user'] != "error contraseña"){
+          include('../templates/header_sin_searchbox_login.html');
+}else{
+    include('../templates/header_sin_searchbox.html');
+}?>
 <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 <meta name="HandheldFriendly" content="true">
