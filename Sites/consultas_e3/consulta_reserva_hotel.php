15785efa49d0ec76a823a7da1e237ce7f013e18a
Richard Morales <rgmorales1@uc.cl>
2020-05-28 19:06:41 -0400

    CE

--- Sites/consultas_e3/consulta_reserva_hotel.php
+++ Sites/consultas_e3/consulta_reserva_hotel.php
@@ -7,7 +7,7 @@ if (isset($_SESSION['user']) && $_SESSION['user'] != "Contraseña erronea" &&
           include('../templates/header_sin_searchbox_login.html');
 }else{
     include('../templates/header_sin_searchbox.html');
-    $mensaje = "Para comprar el ticket debes iniciar s"
+    $mensaje = "Para comprar el ticket debes iniciar sesion";
 }?>
 <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
