79cab985706a6c5273fe316116bef01fb2458b11
Matias Cadile <mjcadile@uc.cl>
2020-06-16 03:43:14 -0400

    implementación de rutas faltantes // tienen errores

--- api/main.py
+++ api/main.py
@@ -1,17 +1,18 @@
 from flask import Flask, render_template, request, abort, json
-from pymongo import MongoClient
+from pymongo import MongoClient, ASCENDING
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
 
 # Para este ejemplo pediremos la id
 # y no la generaremos automáticamente
-USER_KEYS = ['uid', 'name', 'last_name',
-            'occupation', 'follows', 'age']
 
 USER = "grupo12"
 PASS = "grupo12"
 DATABASE = "grupo12"
+
+MSG_KEYS = ["message", "sender", "receptant", "lat", "long", "date"]
+TXT_KEYS = ["desired", "required", "forbidden", "userId"]
 
 # El cliente se levanta en la URL de la wiki
 # URL = "mongodb://grupo12:grupo12@gray.ing.puc.cl/grupo12"
@@ -21,26 +22,20 @@ client = MongoClient(URL)
 # Utilizamos la base de datos del grupo
 db = client["grupo12"]
 
-# Seleccionamos la collección de usuarios y mensajes
+# Creamos los índices para el textsearch
+
+
+# Seleccionamos la collección de usuarios y mensajes // creamos el indice en mensajes
 usuarios = db.users
 mensajes = db.messages
 
-'''
-Usuarios:
-    "uid": <id del usuario>,
-    "name": <nombre>,
-    "description": <descripción del usuario>,
-    "age": <edad>
-
-Mensajes:
-    "date": <fecha del mensaje>,
-    "lat": <latitud>,
-    "long": <longitud>,
-    "message": <mensaje>,
-    "mid": <id del mensaje>,
-    "receptant": <id del receptor>,
-    "sender": <id del emisor>
-'''
+result = mensajes.create_index("messages")
+if result:
+    msje = "se ha creado el indice correctamente"
+    print(msje)
+else:
+    msje = "no se ha podido crear el indice"
+    print(msje)
 
 # Iniciamos la aplicación de flask
 app = Flask(__name__)
@@ -74,7 +69,7 @@ def get_messages():
     uid2 = request.args.get('id2', default=None, type=int)
     print(uid1, uid2)
 
-    if uid1 is None or uid2 is None:
+    if (uid1 is None) or (uid2 is None):
         # Omitir el _id porque no es json serializable
         resultados = list(mensajes.find({}, {"_id": 0}))
         return json.jsonify(resultados)
@@ -82,10 +77,10 @@ def get_messages():
                                        {"_id": 0}))
     mensajes_uid2 = list(mensajes.find({"sender": uid2, "receptant": uid1}, 
                                        {"_id": 0}))
-    print(mensajes_uid1, mensajes_uid2)
+    #print(mensajes_uid1, mensajes_uid2)
     if mensajes_uid1 or mensajes_uid2:
         return json.jsonify(mensajes_uid1 + mensajes_uid2)
-    return "<h2>No hay mensajes intercambiados entre los id dados</h2>"
+    return { "error": f"No hay mensajes entre los usuarios {uid1} y {uid2}"}
 
 
 @app.route("/users/<int:uid>")
@@ -97,7 +92,7 @@ def get_user(uid):
     mensajes_user = list(mensajes.find({"sender": uid}, {"_id": 0}))
     if user:
         return json.jsonify(user + mensajes_user)
-    return "<h2>Usuario no encontrado, intente con otro id</h2>"
+    return { "error": "Usuario no encontrado, intente con otro id"}
 
 
 @app.route("/messages/<int:mid>")
@@ -108,54 +103,118 @@ def get_message(mid):
     mensaje = list(mensajes.find({"mid": mid}, {"_id": 0}))
     if mensaje:
         return json.jsonify(mensaje)
-    return "<h2>Mensaje no encontrado, intente con otro id</h2>"
+    return { "error": "Mensaje no encontrado, intente con otro id"}
 
 
-@app.route("/users", methods=['POST'])
-def create_user():
-    '''
-    Crea un nuevo usuario en la base de datos
-    Se  necesitan todos los atributos de model, a excepcion de _id
-    '''
+@app.route("/text-search", methods=['POST'])
+def text_search():
+    
+    if request.json is None:
+        #  retornamos todos los mensajes
+        resultados = list(mensajes.find({}, {"_id": 0}))
+        return json.jsonify(resultados)
 
-    # En este caso nos entregarán la id del usuario,
-    # Y los datos serán ingresados como json
-    # Body > raw > JSON en Postman
-    data = {key: request.json[key] for key in USER_KEYS}
+    data = {key: request.json[key] for key in TXT_KEYS}
 
-    # El valor de result nos puede ayudar a revisar
-    # si el usuario fue insertado con éxito
-    result = usuarios.insert_one(data)
+    userId = 0
 
-    return json.jsonify({'success': True, 'message': 'Usuario con id 1 creado'})
+    busqueda = ""
 
+    for key in data.keys():
+        if key == "desired":
+            desired = data["desired"]  # lista con palabras que se quieren buscar
+            for frase in desired:
+                busqueda += frase  # agregamos las palabras solo separadas por espacios (buscamos ucrrencia de una o la otra, o ambas)
+                busqueda += " "
+        elif key == "required": 
+            required = data["required"]  # lista con palabras que tienen que estar presentes
+            for frase in required:
+                modificada = "\"{}\"".format(frase)
+                busqueda += modificada  # agregamos frases que si o si deben estar en la busqueda
+                busqueda += " "
+        elif key == "forbidden": 
+            forbidden = data["forbidden"]  # lista con palabras que no pueden estar
+            for frase in forbidden:
+                modificada = "-\"{}\"".format(frase)
+                busqueda += modificada  # agregamos frases que no pueden estar en la busqueda
+                if frase == forbidden[-1]:
+                    break
+                busqueda += " "
+        elif key == "userId":
+            userId = int(data["userId"])  # entero con el id de usuario
+    
+    if len(data.keys()) == 1 and data.keys()[0] == "forbidden":
+        # no retorna nada, ya que son solo palabras prohibidas
+        return json.jsonify({"error": "Solo se entregaron palabras prohibidas, no se ha podido hacer la busqueda."})
 
-@app.route("/test")
-def test():
-    # Obtener un parámero de la URL
-    # Ingresar desde Params en Postman
-    # O agregando ?name=... a URL
-    param = request.args.get('name', False)
-    print("URL param:", param)
+    if userId == 0:
+        #  retornamos el text-search pero para cualquier usuario
+        mensajes_busqueda = list(mensajes.find({"$text": {"$search": busqueda}}, {"_id": 0}))
+        if mensajes_busqueda:
+            return json.jsonify(mensajes_busqueda)
+        return json.jsonify({"error": "No se encontraron referencias a lo solicitado"})
+    else:
+        #  retornamos el text-search pero para el usuario indicado
+        #  mensajes_filter_user = mensajes.find({"sender": userId}, {"_id": 0})
+        
+        #  ACA TIRA EL ERROR
+        mensajes_busqueda = list(mensajes.find({"$text": {"$search": "{}".format(busqueda)}}, {"_id": 0}))
+        #
 
-    # Obtener un header
-    # Ingresar desde Headers en Postman
-    param2 = request.headers.get('name', False)
-    print("Header:", param2)
+        lista_especifica = []
+        for msn in mensajes_busqueda:
+            if int(msn["sender"]) == userId:
+                lista_especifica.append(msn)
+        if lista_especifica:
+            return json.jsonify(lista_especifica)
+        return json.jsonify({"error": "No se encontraron referencias a lo solicitado"})
 
-    # Obtener el body
-    # Ingresar desde Body en Postman
-    body = request.data
-    print("Body:", body)
+@app.route("/messages", methods=['POST'])
+def receive_message():
+    
+    if request.json is None:
+        return json.jsonify({"error": "inserte un mensaje para agregar"})
 
-    return f'''
-            OK
-            <p>parámetro name de la URL: {param}<p>
-            <p>header: {param2}</p>
-            <p>body: {body}</p>
-            '''
-            
+    data = {key: request.json[key] for key in MSG_KEYS}
+    count = mensajes.count_documents({})
+    print(count)
+
+    # venga o no el parametro mid en el json, se agrega igual o se cambia
+    data["mid"] = count + 1
+
+    if len(data.keys()) == (len(MSG_KEYS) + 1):
+        #estan todos los atributos
+        result = mensajes.insert_one(data)
+        if result:
+            msj = "El mensaje se inserto correctamente"
+            success = True
+        else:
+            msj = "No se ha podido insertar el mensaje"
+            success = False
+        return json.jsonify({"success": success, "log": msj})
+    else:
+        faltantes = []
+        for key in MSG_KEYS:
+            if key not in data.keys():
+                faltantes.append(key)
+        msj = "Faltan atributos para poder agregar el mensaje correctamente: {}".format(faltantes)
+        success = False
+        return json.jsonify({"success": success, "log": msj})
+
+@app.route("/messages/<int:mid>", methods=['DELETE'])
+def delete_message(mid):
+    result = mensajes.delete_one({"mid": int(mid)})
+    if not result:
+        msj = "El id no existe. No se ha podido borrar el mensaje"
+        success = False
+    else:
+        msj = "Se ha borrado correctamente el mensaje"
+        success = True
+
+    return json.jsonify({"success": success, "log": msj})
+
 if __name__ == "__main__":
     #app.run()
     app.run(debug=True) # Para debuggear!
 # ¡Mucho ánimo y éxito! ¡Saludos! :D
+
