cc3a79317df36197202a2ca1b92dd3b64e91466f
Matias Cadile <mjcadile@uc.cl>
2020-07-05 03:59:47 -0400

    request api prueba

--- Sites/users/msj_enviados.php
+++ Sites/users/msj_enviados.php
@@ -30,32 +30,15 @@ foreach ($usuario as $us) {
       $mid = $array[4];
       $receptant = $array[5];
       $sender = $array[6];
-      echo "
-      <div class='container-fluid'>
-        <div class='row'>
-          <p> Mensaje #$mid </p>
-          <div class='col-4'>
-            <div class='list-group' id='list-tab' role='tablist'>
-              <a class='list-group-item list-group-item-action active' id='list-home-list' data-toggle='list' href='#list-home' role='tab' aria-controls='home'>Información</a>
-              <a class='list-group-item list-group-item-action' id='list-profile-list' data-toggle='list' href='#list-profile' role='tab' aria-controls='profile'>Mensaje</a>
-              <a class='list-group-item list-group-item-action' id='list-messages-list' data-toggle='list' href='#list-messages' role='tab' aria-controls='messages'>Ubicación</a>
-            </div>
-          </div>
-          <div class='col-8'>
-            <div class='tab-content' id='nav-tabContent'>
-              <div class='tab-pane fade show active' id='list-home' role='tabpanel' aria-labelledby='list-home-list'>
-              <p>Fecha de emisión: $fecha</p>
-              <p>ID de emisor: $sender</p>
-              <p>ID de destinatario: $receptant</p>
-              </div>
-              <div class='tab-pane fade' id='list-profile' role='tabpanel' aria-labelledby='list-profile-list'>$mensaje</div>
-              <div class='tab-pane fade' id='list-messages' role='tabpanel' aria-labelledby='list-messages-list'>Longitud: $long  |  Latitud: $lat</div>
-            </div>
-          </div>
-        </div>
-      </div>
-      ";
-    }
-  }
+      echo "<div class='container-fluid>
+            <h3>$fecha</h3>
+            <h3>$lat</h3>
+            <h3>$long</h3>
+            <h3>$message</h3>
+            <h3>$mid</h3>
+            <h3>$receptant</h3>
+            <h3>$sender</h3>
+            </div>";
+     
   ?>
   
