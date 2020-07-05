601d1a9f74427b68ae060418bc87f2bb9b228d52
Matias Cadile <mjcadile@uc.cl>
2020-07-05 03:47:42 -0400

    request api prueba

--- Sites/users/msj_enviados.php
+++ Sites/users/msj_enviados.php
@@ -18,9 +18,42 @@ foreach ($usuario as $us) {
   
   $response = file_get_contents('https://lovely-glacier-09476.herokuapp.com/users/1');
   $response = json_decode($response, true);
-  foreach($response as $array) {
-    foreach ($array as $item) {
-      echo "<p>$item</p>";
+  if (count($response) == 1) {
+    echo "<h1>$response</h1>"
+  } else {
+    $mensajes = array_slice($response, 1)
+    foreach($mensajes as $array) {
+      $fecha = $array[0];
+      $lat = $array[1];
+      $long = $array[2];
+      $message = $array[3];
+      $mid = $array[4];
+      $receptant = $array[5];
+      $sender = $array[6];
+      echo "
+        <div class='row'>
+          <p> Mensaje #$mid </p>
+          <div class='col-4'>
+            <div class='list-group' id='list-tab' role='tablist'>
+              <a class='list-group-item list-group-item-action active' id='list-home-list' data-toggle='list' href='#list-home' role='tab' aria-controls='home'>Información</a>
+              <a class='list-group-item list-group-item-action' id='list-profile-list' data-toggle='list' href='#list-profile' role='tab' aria-controls='profile'>Mensaje</a>
+              <a class='list-group-item list-group-item-action' id='list-messages-list' data-toggle='list' href='#list-messages' role='tab' aria-controls='messages'>Ubicación</a>
+            </div>
+          </div>
+          <div class='col-8'>
+            <div class='tab-content' id='nav-tabContent'>
+              <div class='tab-pane fade show active' id='list-home' role='tabpanel' aria-labelledby='list-home-list'>
+              <p>Fecha de emisión: $fecha</p>
+              <p>ID de emisor: $sender</p>
+              <p>ID de destinatario: $receptant</p>
+              </div>
+              <div class='tab-pane fade' id='list-profile' role='tabpanel' aria-labelledby='list-profile-list'>"$mensaje"</div>
+              <div class='tab-pane fade' id='list-messages' role='tabpanel' aria-labelledby='list-messages-list'>Longitud: $long  |  Latitud: $lat</div>
+            </div>
+          </div>
+        </div
+      ";
     }
   }
-?>
+  ?>
+  
