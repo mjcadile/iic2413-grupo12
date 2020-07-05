628d79791e6d4231924ac4430c6b249c13041c43
Richard Morales <rgmorales1@uc.cl>
2020-07-05 18:06:54 -0400

    A

--- Sites/users/ubicaciones_mensajes.php
+++ Sites/users/ubicaciones_mensajes.php
@@ -1,38 +1,46 @@
 <?php
 session_start();
 include('../templates/header_sin_searchbox_login_msj.html');
-if (isset($_SESSION['user']) && $_SESSION['user'] != "Contraseña erronea" && 
-        $_SESSION['user'] != "Usuario no encontrado" && $_SESSION['user'] != "error username" && 
-        $_SESSION['user'] != "error contraseña"){
-    include('templates/header_login.html');
-}else{
-    include('templates/header.html');
-}?>
+require("../config/conexion.php");  
+$username = $_SESSION["user"];
+$query_usuario = "SELECT Usuarios.uid, Usuarios.nombre_usuario FROM Usuarios WHERE username = '$username';";
+$result_user = $db_19 -> prepare($query_usuario);
+$result_user -> execute();
+$usuario = $result_user -> fetchAll(); 
+foreach ($usuario as $us) {
+    $uid = $us[0];
+    $nombre = $us[1];
+}
+require("../config/conexion.php");
+$fecha_entrada = $_POST["start"];
+$fecha_salida = $_POST["finish"];
 
-<?php
-    require("../config/conexion.php");
-    $fecha_entrada = $_POST["start"];
-    $fecha_salida = $_POST["finish"];
-    # Las ubicaciones son con latitud y longitus
 ?>
-
+<meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta name="HandheldFriendly" content="true">
 
 <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"
    integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
    crossorigin=""/>
-<body>
 
-   <div id="mapid" style="height: 300px"></div>
+<img src="https://wallpaperaccess.com/full/2048343.jpg" id="bg" alt="">
+<br>
+
+
+    
+
+
+
+
+<div id="mapid" style="height: 300px"></div>
    
-</body>
+
 
 <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
    integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
    crossorigin=""></script>
 
-<meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
-<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-<meta name="HandheldFriendly" content="true">
 <script> 
     var map = L.map('mapid').setView([51.505, -0.09], 13); 
 
