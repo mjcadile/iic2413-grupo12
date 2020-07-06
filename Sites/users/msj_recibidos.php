a8a642edb0a1c13be11a82db7e6665301c1030eb
Richard Morales <rgmorales1@uc.cl>
2020-07-05 22:00:54 -0400

    A

--- Sites/users/msj_recibidos.php
+++ Sites/users/msj_recibidos.php
@@ -15,61 +15,58 @@ $query_usuarios = "SELECT Usuarios.uid, Usuarios.nombre_usuario FROM Usuarios;";
 $result_users = $db_19 -> prepare($query_usuarios);
 $result_users -> execute();
 $usuarios = $result_users -> fetchAll(); 
-foreach ($usuarios as $us) {
-    $uid_sender = $us[0];
-    $nombre_sender = $us[1];
-    $contador = 0;
+  
 
-    $response = file_get_contents('https://lovely-glacier-09476.herokuapp.com/messages?id1=' . $uid . '&id2=' . $uid_sender);
-    echo $response;
-    $response = json_decode($response, true);
-    $mensajes = array_slice($response, 1);
-    foreach($mensajes as $array) {
-      $atributos = array();
-      foreach ($array as $item) {
-        array_push($atributos, $item);
-      }
-      $fecha = $atributos[0];
-      $lat = $atributos[1];
-      $long = $atributos[2];
-      $message = $atributos[3];
-      $mid = $atributos[4];
-      $receptant = $atributos[5];
-      $sender = $atributos[6];
-      if ($uid == $receptant){
-        $contador += 1;
-        echo "<div class='card text-center'>
-                <div class='card-header'>
-                  Mensaje #$mid
-                </div>
-                <div class='card-body'>
-                  <h5 class='card-title'>$message</h5>
-                  <p class='card-text'>Ubicación: Lat: $lat  |  Long: $long</p>
-                  <p class='card-text'>ID remitente: $sender</p>
-                  <p class='card-text'>ID destinatario: $receptant</p>
-                </div>
-                <div class='card-footer text-muted'>
-                  Fecha de emisión: $atributos[0]
-                </div>
-              </div
-              <br>
-              <br>";
-      }
-    }
+$contador = 0;
+$response = file_get_contents('https://lovely-glacier-09476.herokuapp.com/messages');
+echo $response;
+$response = json_decode($response, true);
+$mensajes = array_slice($response, 1);
+foreach($mensajes as $array) {
+  $atributos = array();
+  foreach ($array as $item) {
+    array_push($atributos, $item);
   }
-  if ($contador == 0 || $contador == 1) {
-    echo "<div class='jumbotron'>
-            <h1 class='display-4'>No tienes mensajes enviados!</h1>
-            <p class='lead'>Lo sentimos, al parecer aún no le envías mensajes a nadie.</p>
-            <hr class='my-4'>
-            <p>Haz click aquí para utilizar nuestro servicio de mensajería.</p>
-            <p class='lead'>
-              <a class='btn btn-primary btn-lg' href='#' role='button'>Enviar mensajes</a>
-              <a class='btn btn-primary btn-lg' href='../index.php' role='button'>Home</a>
-            </p>
-          </div>
+  $fecha = $atributos[0];
+  $lat = $atributos[1];
+  $long = $atributos[2];
+  $message = $atributos[3];
+  $mid = $atributos[4];
+  $receptant = $atributos[5];
+  $sender = $atributos[6];
+  if ($uid == $receptant){
+    $contador += 1;
+    echo "<div class='card text-center'>
+            <div class='card-header'>
+              Mensaje #$mid
+            </div>
+            <div class='card-body'>
+              <h5 class='card-title'>$message</h5>
+              <p class='card-text'>Ubicación: Lat: $lat  |  Long: $long</p>
+              <p class='card-text'>ID remitente: $sender</p>
+              <p class='card-text'>ID destinatario: $receptant</p>
+            </div>
+            <div class='card-footer text-muted'>
+              Fecha de emisión: $atributos[0]
+            </div>
+          </div
+          <br>
           <br>";
   }
+}
+if ($contador == 0 || $contador == 1) {
+  echo "<div class='jumbotron'>
+          <h1 class='display-4'>No tienes mensajes enviados!</h1>
+          <p class='lead'>Lo sentimos, al parecer aún no le envías mensajes a nadie.</p>
+          <hr class='my-4'>
+          <p>Haz click aquí para utilizar nuestro servicio de mensajería.</p>
+          <p class='lead'>
+            <a class='btn btn-primary btn-lg' href='#' role='button'>Enviar mensajes</a>
+            <a class='btn btn-primary btn-lg' href='../index.php' role='button'>Home</a>
+          </p>
+        </div>
+        <br>";
+}
   ?>
 <br>
 <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
