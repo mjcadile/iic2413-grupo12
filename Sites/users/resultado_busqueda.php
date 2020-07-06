9edd1909508757088f876c243162834f3f0a6d02
Sebastián Burgos <snburgos@uc.cl>
2020-07-06 18:21:43 -0400

    search

--- Sites/users/resultado_busqueda.php
+++ Sites/users/resultado_busqueda.php
@@ -6,16 +6,34 @@ require("../config/conexion.php");
 $data = $_POST['search'];
 
 
-$client = new Client();
+$curl = curl_init();
 
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://lovely-glacier-09476.herokuapp.com/messages",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_POSTFIELDS => "Body goes here",
+  CURLOPT_HTTPHEADER => array(
+    "cache-control: no-cache",
+    "content-type: application/x-www-form-urlencoded"
+    'x-api-key: XXXXXX',
+  ),
+));
 
-    $response = $client->request('POST', 'https://lovely-glacier-09476.herokuapp.com/messages', [
-            'form_params' => [
-                'format' => json_encode($data),
-            ],
-        ]);
-    $response_data = json_decode($response->getBody()->getContents());
-    dd($response_data);
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
+}
 
 
 ?>
