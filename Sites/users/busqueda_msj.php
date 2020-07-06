de7b28557d189bcc0559a580a255dcdd9f102ef7
Sebastián Burgos <snburgos@uc.cl>
2020-07-06 16:27:34 -0400

    search

--- Sites/users/busqueda_msj.php
+++ Sites/users/busqueda_msj.php
@@ -6,7 +6,7 @@ require("../config/conexion.php");
 echo "<div class='card'>
       <br>
           <div class='card-body'>
-            <h2 class='card-title'>Mensajes recibidos</h2>
+            <h1 class='card-title'>Frase que deseablemente esté en el mensaje</h1>
           </div>
           <br>
       </div>
@@ -22,52 +22,7 @@ echo "<html>
         </body>
       </html>";
 
-foreach($mensajes as $array) {
-  $atributos = array();
-  foreach ($array as $item) {
-    array_push($atributos, $item);
-  }
-  $fecha = $atributos[0];
-  $lat = $atributos[1];
-  $long = $atributos[2];
-  $message = $atributos[3];
-  $mid = $atributos[4];
-  $receptant = $atributos[5];
-  $sender = $atributos[6];
-  if ($uid == $receptant){
-    $contador += 1;
-    echo "<div class='card text-center'>
-            <div class='card-header'>
-              Mensaje #$mid
-            </div>
-            <div class='card-body'>
-              <h5 class='card-title'>$message</h5>
-              <p class='card-text'>Ubicación: Lat: $lat  |  Long: $long</p>
-              <p class='card-text'>ID remitente: $sender</p>
-              <p class='card-text'>ID destinatario: $receptant</p>
-            </div>
-            <div class='card-footer text-muted'>
-              Fecha de emisión: $atributos[0]
-            </div>
-          </div>
-          <br>
-          <br>";
-  }
-}
-if ($contador == 0 || $contador == 1) {
-  echo "<div class='jumbotron'>
-          <h1 class='display-4'>No tienes mensajes recibidos!</h1>
-          <p class='lead'>Lo sentimos, al parecer aún tienes mensajes recibidos.</p>
-          <hr class='my-4'>
-          <p>Haz click aquí para utilizar nuestro servicio de mensajería.</p>
-          <p class='lead'>
-            <a class='btn btn-primary btn-lg' href='#' role='button'>Enviar mensajes</a>
-            <a class='btn btn-primary btn-lg' href='../index.php' role='button'>Home</a>
-          </p>
-        </div>
-        <br>";
-}
-  ?>
+?>
 <br>
 <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
