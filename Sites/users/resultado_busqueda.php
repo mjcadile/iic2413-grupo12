8468202e7b387da1298f47789935bc0b22102fa1
Sebastián Burgos <snburgos@uc.cl>
2020-07-06 19:21:34 -0400

    search

--- Sites/users/resultado_busqueda.php
+++ Sites/users/resultado_busqueda.php
@@ -9,8 +9,8 @@ $data = $_POST['search'];
 
 
 $curl = curl_init();
-$consulta['data'] = ["desired" => ["Metallica", "canción"],
-                     "required" => ["Hola"]];
+$consulta = ["desired" => ["Metallica", "canción"],
+             "required" => ["Hola"]];
 
 
 
@@ -23,7 +23,7 @@ curl_setopt_array($curl, array(
   CURLOPT_TIMEOUT => 30,
   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
   CURLOPT_CUSTOMREQUEST => "GET",
-  CURLOPT_POSTFIELDS => json_encode($consulta['data']),
+  CURLOPT_POSTFIELDS => json_encode($consulta),
   CURLOPT_HTTPHEADER => array(),
 ));
 
