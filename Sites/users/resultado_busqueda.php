eee0ac6c8d97b480a438bc0f92993d799c2cedb3
Sebastián Burgos <snburgos@uc.cl>
2020-07-06 19:00:21 -0400

    search

--- Sites/users/resultado_busqueda.php
+++ Sites/users/resultado_busqueda.php
@@ -16,7 +16,7 @@ curl_setopt_array($curl, array(
   CURLOPT_TIMEOUT => 30,
   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
   CURLOPT_CUSTOMREQUEST => "GET",
-  CURLOPT_POSTFIELDS => json_encode('{
+  CURLOPT_POSTFIELDS => '{
                           "required": ["Hola"]
                          }'),
   CURLOPT_HTTPHEADER => array(),
