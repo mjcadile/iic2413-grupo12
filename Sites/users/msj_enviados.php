9c9f881855cf60316f96cacf10be61edbb589c1c
Richard Morales <rgmorales1@uc.cl>
2020-07-08 18:11:45 -0400

    Arreglado

--- Sites/users/msj_enviados.php
+++ Sites/users/msj_enviados.php
@@ -11,19 +11,17 @@ foreach ($usuario as $us) {
     $uid = $us[0];
     $nombre = $us[1];
 }
-?>
-<!--?$query_uid = "SELECT uid FROM Usuarios WHERE Usuarios.username = '$username';";?-->
-<meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
-<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-<meta name="HandheldFriendly" content="true">
-<img src="https://wallpaperaccess.com/full/2048343.jpg" id="bg" alt="">
-<br>
-<?php 
-  $contador = 0;
-  $response = file_get_contents('https://lovely-glacier-09476.herokuapp.com/users/'.$uid);
-  $response = json_decode($response, true);
-  $mensajes = array_slice($response, 1);
-  echo "<div class='card'>
+$query_usuarios = "SELECT Usuarios.uid, Usuarios.nombre_usuario FROM Usuarios;";
+$result_users = $db_19 -> prepare($query_usuarios);
+$result_users -> execute();
+$usuarios = $result_users -> fetchAll(); 
+  
+
+$contador = 0;
+$response = file_get_contents('https://lovely-glacier-09476.herokuapp.com/messages');
+$response = json_decode($response, true);
+$mensajes = array_slice($response, 1);
+echo "<div class='card'>
       <br>
           <div class='card-body'>
             <h2 class='card-title'>Mensajes enviados</h2>
@@ -31,19 +29,20 @@ foreach ($usuario as $us) {
           <br>
       </div>
       <br>";
-  foreach($mensajes as $array) {
+foreach($mensajes as $array) {
+  $atributos = array();
+  foreach ($array as $item) {
+    array_push($atributos, $item);
+  }
+  $fecha = $atributos[0];
+  $lat = $atributos[1];
+  $long = $atributos[2];
+  $message = $atributos[3];
+  $mid = $atributos[4];
+  $receptant = $atributos[5];
+  $sender = $atributos[6];
+  if ($uid == $sender){
     $contador += 1;
-    $atributos = array();
-    foreach ($array as $item) {
-      array_push($atributos, $item);
-    }
-    $fecha = $atributos[0];
-    $lat = $atributos[1];
-    $long = $atributos[2];
-    $message = $atributos[3];
-    $mid = $atributos[4];
-    $receptant = $atributos[5];
-    $sender = $atributos[6]; 
     echo "<div class='card text-center'>
             <div class='card-header'>
               Mensaje #$mid
@@ -57,22 +56,29 @@ foreach ($usuario as $us) {
             <div class='card-footer text-muted'>
               Fecha de emisión: $atributos[0]
             </div>
-          </div
+          </div>
           <br>
           <br>";
   }
-  if ($contador == 0 || $contador == 1) {
-    echo "<div class='jumbotron'>
-            <h1 class='display-4'>No tienes mensajes enviados!</h1>
-            <p class='lead'>Lo sentimos, al parecer aún no le envías mensajes a nadie.</p>
-            <hr class='my-4'>
-            <p>Haz click aquí para utilizar nuestro servicio de mensajería.</p>
-            <p class='lead'>
-              <a class='btn btn-primary btn-lg' href='enviar_msj.php' role='button'>Enviar mensajes</a>
-              <a class='btn btn-primary btn-lg' href='../index.php' role='button'>Home</a>
-            </p>
-          </div>
-          <br>";
-  }
+}
+if ($contador == 0 || $contador == 1) {
+  echo "<div class='jumbotron'>
+          <h1 class='display-4'>No tienes mensajes recibidos!</h1>
+          <p class='lead'>Lo sentimos, al parecer aún tienes mensajes recibidos.</p>
+          <hr class='my-4'>
+          <p>Haz click aquí para utilizar nuestro servicio de mensajería.</p>
+          <p class='lead'>
+            <a class='btn btn-primary btn-lg' href='enviar_msj.php' role='button'>Enviar mensajes</a>
+            <a class='btn btn-primary btn-lg' href='../index.php' role='button'>Home</a>
+          </p>
+        </div>
+        <br>";
+}
   ?>
-  <?php include('../templates/footer.html');?>
+<br>
+<meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta name="HandheldFriendly" content="true">
+<img src="https://wallpaperaccess.com/full/2048343.jpg" id="bg" alt="">
+
+<?php include('../templates/footer.html');?>
